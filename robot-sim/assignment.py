2ad9a50ad820249b5237de736476f4496cbadfb7
danielerialdi <daniele.rialdi@gmail.com>
2023-11-11 10:14:21 +0000

    Refactoring on assignment.py done, sped up the robot a little

--- robot-sim/assignment.py
+++ robot-sim/assignment.py
@@ -84,26 +84,6 @@ def find_token():
     else:
         return dist, rot_y
         
-def find_token_from_list(array):
-    """
-    Function to find the closest token in a list of offsets
-
-    Returns:
-	dist (float): distance of the closest token (-1 if no token is detected)
-	rot_y (float): angle between the robot and the token (-1 if no token is detected)
-    """
-    dist=100
-    for token in R.see():
-    	for i in range(1,len(array)-1): #1 because the first is the reference, could CHANGE
-    		if token.info.offset == array[i]:
-    			if token.dist < dist:
-            			dist=token.dist
-	    			rot_y=token.rot_y
-	    			break
-    if dist==100:
-        return -1, -1
-    else:
-        return dist, rot_y
 
 def find_that_token(m):
     """
@@ -120,7 +100,7 @@ def find_that_token(m):
             		dist=token.dist
 	    		rot_y=token.rot_y
     	if dist==100:
-        	turn(5,0.5)
+        	turn(10,0.2)
     
     return dist, rot_y
 def min_dist():
@@ -158,15 +138,15 @@ def diam_dist():
 					dist, index = min_dist()
 				if(len(array_all_token) == len(token_array)):
 					temp = False
-					print("FINALLL")
+					# print("FINAL")
 				if(m.info.offset not in array_all_token):
 					array_all_token.append(m.info.offset)
 					array_dist.append(m.dist)
 					update_token_array(m.info.offset)
 				
-			print(array_all_token)
+			# print(array_all_token)
 	i = array_dist.index(max(array_dist))
-	print(str(max(array_dist))+" "+ str(array_all_token[i]))
+	# print(str(max(array_dist))+" "+ str(array_all_token[i]))
 	return max(array_dist), array_all_token[i]
 			
 		
@@ -185,27 +165,27 @@ def first_iteration():
 				while rot_y > a_th:
 					if rot_y < -a_th: 
 						print("Left a bit...")
-						turn(-2, 0.5)
+						turn(-4, 0.3)
 	    				elif rot_y > a_th:
 						print("Right a bit...")
-						turn(+2, 0.5)
-					print(rot_y)
+						turn(+4, 0.3)
+					# print(rot_y)
 					dist, rot_y = find_that_token(m.info.offset)
 					if -a_th<rot_y < a_th:
-						print("Offset "+str(m.info.offset))
+						# print("Offset "+str(m.info.offset))
 						first_iteration_condition = True
 						break
 	global token_array
 	dist_max = max_dist()
 	dist_min = min_dist()
 	diameter = dist_max[0]-dist_min[0]
-	print(str(dist_min))
+	# print(str(dist_min))
 	for i in range(0,len(token_array)-1):
 		#token_array.pop("dist_min[1]")
 		if token_array[i] == dist_min[1]:
 			token_array.pop(i)
 	token_array.insert(0, dist_min[1])
-	print(token_array)
+	# print(token_array)
 	global radius
 	radius = diameter/2.0
 	param = diameter
@@ -213,24 +193,16 @@ def first_iteration():
 		param, rot_y = find_that_token(indexD)
 		dist_min = min_dist()
 		param = param-dist_min[0]
-		drive(10,0.3)
+		drive(20,0.1)
 def main():
 	markers = R.see()
 	global token_array
 	global total_counter
-	istance = 0
+	instance = 0
 	while_condition = 0
 	second_iteration = 0
 	avg = 0
-	#for m in markers:
-		#rot_y = m.centre.polar.rot_y
-		#print(str(rot_y))
-    	#	if m.info.marker_type in (MARKER_TOKEN_GOLD, MARKER_TOKEN_SILVER, MARKER_ARENA):
-        #		#print(" - Token {0} is {1} metres away".format( m.info.offset, m.dist ))
-        #	elif m.info.marker_type == MARKER_ARENA:
-        #		print(" - Arena marker {0} is {1} metres away".format( m.info.offset, m.dist ))
 	while 1:
-	    #if time == 0:
 	    dist, rot_y = find_token()
 	    while (R.see() and while_condition == 0):
 	    	marker = R.see()
@@ -247,40 +219,48 @@ def main():
 		    		update_token_array(m.info.offset)
 	    	turn(10,1)
 	    while((not R.see()) and while_condition == 0):
-	    	print(str(R.see()))
+	    	# print(str(R.see()))
 	    	turn(30,1)
 	    while_condition = 1
-	    #	dist, rot_y = find_token_from_list(token_array)
 	    if not R.see() and total_counter == len(token_array):
 		print("My work here is done!!")
 		exit()
 	    elif dist <d_th: 
-		print("Found it!")
+		# print("Found it!")
 		R.grab() # if we are close to the token, we grab it.
 		# FIRST ITERATION ONLY:
 		global radius
-		if istance == 0:
+		if instance == 0:
 			first_iteration()
-			istance = 1
+			instance = 1
 		else:
 			dist, rot_y = find_that_token(token_array[0])
-			trashold = 0.55
-			while(dist > trashold):
+			threshold = 0.55
+			while(dist > threshold):
 				drive(10,0.3)
 				dist, rot_y = find_that_token(token_array[0])
-				print(str(dist)+ " with token " +str(token_array[0]) )
+				if -a_th<= rot_y <= a_th: # if the robot is well aligned with the token, we go forward
+				    print("Ah, here we are!.")
+				    drive(20, 0.2)
+	    			elif rot_y < -a_th:
+				    print("Left a bit...")
+				    turn(-4, 0.3)
+	    			elif rot_y > a_th:
+				    print("Right a bit...")
+				    turn(+4, 0.3)
+				# print(str(dist)+ " with token " +str(token_array[0]) )
 				
 		if(R.release()):
 			R.release()
 			total_counter = total_counter + 1
-			print("counter" + str(total_counter))
+			# print("counter" + str(total_counter))
 		drive(-20,1)
 		if(second_iteration == 1):
 			turn(-10,avg)
-			print(str(avg))
+			# print(str(avg))
 			while(not R.see() and total_counter != len(token_array)):
 				turn(10,0.2)
-				print("CONDITION"+str(total_counter)+ " " +str(len(token_array)))
+				# print("CONDITION"+str(total_counter)+ " " +str(len(token_array)))
 		if(second_iteration == 0):
 			start_time = time.time()
 			while(R.see() and second_iteration == 0):
@@ -293,24 +273,17 @@ def main():
 			# Even if we assume a time = 0 to check the condition of the while loop, the time calculated
 			# is given for going further (in fact we have to come back to see the marker again)
 			second_iteration = 1
-			print(str(avg))
-			
-		
-		#turn(-20, 2)
+			# print(str(avg))
 		dist, rot_y = find_token()
-		#while dist < radius:
-		#	turn(5,1)
-		#	print(rot_y)
-		#	dist, rot_y = find_token()
+		
 			
 	    elif -a_th<= rot_y <= a_th: # if the robot is well aligned with the token, we go forward
 		print("Ah, here we are!.")
-		drive(10, 0.5)
+		drive(20, 0.2)
 	    elif rot_y < -a_th: # if the robot is not well aligned with the token, we move it on the left or on the right
 		print("Left a bit...")
-		turn(-2, 0.5)
+		turn(-4, 0.3)
 	    elif rot_y > a_th:
 		print("Right a bit...")
-		turn(+2, 0.5)
+		turn(+4, 0.3)
 main()
-    
